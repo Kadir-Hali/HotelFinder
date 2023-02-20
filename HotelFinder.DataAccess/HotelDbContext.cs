1fc53236010e49219c6e9560b60f687ef997ecd3
Kadir Hali <kadirhali07@gmail.com>
2023-02-20 17:49:26 +0300

    Add project files.

---
+++ HotelFinder.DataAccess/HotelDbContext.cs
@@ -0,0 +1,21 @@
+﻿using HotelFinder.Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelFinder.DataAccess
+{
+    public class HotelDbContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\MSSQLLocalDB;Database=HotelDb;Trusted_Connection=true");
+        }
+
+        public DbSet<Hotel> Hotels { get; set; }
+    }
+}
---
+++ HotelFinder.Entities/Concrete/Hotel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HotelFinder.Entities.Abstract;
+
+namespace HotelFinder.Entities.Concrete
+{
+    public class Hotel:IEntity
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [StringLength(50)]
+        public string Name { get; set; }
+
+        [StringLength(50)]
+        public string City { get; set; }
+    }
+}
